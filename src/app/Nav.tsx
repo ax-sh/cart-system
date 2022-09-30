e17c176b8481cfe0ef7d5d5f8260f65bcc9caa70
axm <56112475+ax-sh@users.noreply.github.com>
2022-09-30 17:24:11 +0545

    fix: nav icon

--- src/app/Nav.tsx
+++ src/app/Nav.tsx
@@ -7,18 +7,20 @@ import { Link, NavLink } from "react-router-dom";
 
 function CartIcon() {
   const cartItems = useSelector((state: RootState) => state.cart.cartItems);
-  const cartItemsCount = cartItems.length;
+  const maxCartItemsLength = cartItems.length > 99;
+  const cartItemsCount = maxCartItemsLength ? "99+" : cartItems.length;
   return (
     <NavLink to={"/cart"} className={"h-full relative w-10"}>
       <ShoppingCartIcon color={"#fff"} height={"100%"} />
 
       <div
         className={clsx(
-          "inline-flex absolute -top-2 -right-2 justify-center items-center w-6 h-6",
-          "text-xs font-bold",
+          "inline-flex absolute -top-2 -right-2 justify-center items-center w-6 h-6 bg-red-600",
+          "font-bold",
           "text-white bg-red",
           "rounded-full",
-          "border-2 border-white dark:border-gray-900"
+          "border-2 border-white dark:border-gray-900",
+          maxCartItemsLength ? "text-[0.5rem]" : "text-xs"
         )}
       >
         {cartItemsCount}
