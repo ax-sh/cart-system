64f7046df1c1ade7a2a589c7a3fbe11805c003bb
axm <56112475+ax-sh@users.noreply.github.com>
2022-10-01 00:40:05 +0545

    fix: error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,33 @@
 import { Layout } from "../app/App";
 import ProductView from "../app/features/ProductsView";
-// import { addToCart } from "../app/features/Cart/cart.slice";
-// import { useDispatch } from "react-redux";
+import { addToCart } from "../app/features/Cart/cart.slice";
+import { useDispatch } from "react-redux";
 import { useAddToCartMutation } from "../app/features/Cart/cart.api";
 
 function Hero() {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const [apiAddToCart] = useAddToCartMutation({
     fixedCacheKey: "shared-update-post",
   });
+
+  function handleAddToCart() {
+    return async () => {
+      const req = await apiAddToCart({ data: "uo" });
+      //@ts-ignore
+      if (!req.error) {
+        console.log("[ADD_TO_CART_REQUEST]", req, 7777);
+        dispatch(addToCart("qw"));
+      }
+    };
+  }
+
   return (
     <section className={"container mx-auto py-50"}>
       <div className={"flex flex-col gap-10 items-start"}>
         <h6 className={"text-[#ffd500] text-4xl"}>Classic Yellow Look</h6>
         <button
           className={"bg-[#ffd500] text-[#15151e] px-6 py-2 rounded-md"}
-          onClick={() => apiAddToCart({ data: "uo" })}
-          // onClick={() => dispatch(addToCart("qw"))}
+          onClick={handleAddToCart()}
         >
           Add To cart
         </button>
