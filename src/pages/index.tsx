cc4435b8a26a01921a2dd0624428afe06a471bc6
axm <56112475+ax-sh@users.noreply.github.com>
2022-10-01 03:10:20 +0545

    fix: api multicall

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,28 +3,20 @@ import ProductView from "../app/features/ProductsView";
 import { addToCart } from "../app/features/Cart/cart.slice";
 import { useDispatch } from "react-redux";
 import { useAddToCartMutation } from "../app/features/Cart/cart.api";
-import {
-  Toast,
-  ToastAction,
-  ToastDescription,
-  ToastTitle,
-} from "../app/features/Toast";
 
 function Hero() {
   const dispatch = useDispatch();
-  const [apiAddToCart] = useAddToCartMutation({
+  const [apiAddToCart, { isLoading }] = useAddToCartMutation({
     fixedCacheKey: "shared-update-post",
   });
 
-  function handleAddToCart() {
-    return async () => {
-      const req = await apiAddToCart({ data: "uo" });
-      //@ts-ignore
-      if (!req.error) {
-        console.log("[ADD_TO_CART_REQUEST]", req, 7777);
-        dispatch(addToCart("qw"));
-      }
-    };
+  async function handleAddToCart() {
+    const req = await apiAddToCart({ data: "uo" });
+    //@ts-ignore
+    if (!req.error) {
+      console.log("[ADD_TO_CART_REQUEST]", req, 7777);
+      dispatch(addToCart("qw"));
+    }
   }
 
   return (
@@ -32,8 +24,9 @@ function Hero() {
       <div className={"flex flex-col gap-10 items-start"}>
         <h6 className={"text-[#ffd500] text-4xl"}>Classic Yellow Look</h6>
         <button
+          disabled={isLoading}
           className={"bg-[#ffd500] text-[#15151e] px-6 py-2 rounded-md"}
-          onClick={handleAddToCart()}
+          onClick={handleAddToCart}
         >
           Add To cart
         </button>
@@ -47,24 +40,6 @@ function Page() {
     <Layout>
       <Hero />
       <ProductView />
-      <Toast
-        open={true}
-        // onOpenChange={}
-      >
-        <ToastTitle>Scheduled: Catch up</ToastTitle>
-        <ToastDescription asChild>
-          Hello
-          {/*<time dateTime={eventDateRef.current.toISOString()}>*/}
-          {/*  {prettyDate(eventDateRef.current)}*/}
-          {/*</time>*/}
-        </ToastDescription>
-        <ToastAction asChild altText="Goto schedule to undo">
-          Yoooo
-          {/*<Button variant="green" size="small">*/}
-          {/*  Undo*/}
-          {/*</Button>*/}
-        </ToastAction>
-      </Toast>
     </Layout>
   );
 }
