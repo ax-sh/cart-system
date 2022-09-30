98b0a3bd5b6ecc134c219f976fa5062dda8c52c3
axm <56112475+ax-sh@users.noreply.github.com>
2022-09-30 16:43:14 +0545

    feat: add delete functionality

--- src/app/features/Cart/index.tsx
+++ src/app/features/Cart/index.tsx
@@ -7,14 +7,21 @@ function Cart() {
   const dispatch = useDispatch();
   const cartItems = useSelector((state: RootState) => state.cart.cartItems);
   return (
-    <section className={"container mx-auto"}>
+    <section className={"container mx-auto grid grid-cols-3 gap-5"}>
       {cartItems.map((item, index) => {
         return (
-          <div key={index}>
-            {item.name} {item.id}
-            <button onClick={() => dispatch(removeFromCart(item.id))}>
-              <XMarkIcon className={"h-5 w-5"} />
-            </button>
+          <div
+            key={index}
+            className={"h-20 rounded-md border-2 border-white p-2"}
+          >
+            <div className={"flex justify-end"}>
+              <button onClick={() => dispatch(removeFromCart(item.id))}>
+                <XMarkIcon className={"h-5 w-5"} />
+              </button>
+            </div>
+            <div className={"px-5 text-xl"}>
+              {item.name} {item.id}
+            </div>
           </div>
         );
       })}
