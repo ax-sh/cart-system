51502514f8255fea55543687750cb972e08875b7
axm <56112475+ax-sh@users.noreply.github.com>
2022-09-30 15:14:02 +0545

    feat:  add redux

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,13 @@ import App from "./app/App";
 import "./index.scss";
 import "virtual:windi.css";
 import "virtual:windi-devtools";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
