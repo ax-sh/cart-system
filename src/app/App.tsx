c45d21418f116a31c993dd51382fa5da2570bf29
axm <56112475+ax-sh@users.noreply.github.com>
2022-09-30 15:08:59 +0545

    feat: add cart icon

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,35 @@
 import React from "react";
 import "./App.scss";
+import { ShoppingCartIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
+
+function CartIcon() {
+  const cartItemsCount = 60;
+  return (
+    <button className={"relative"}>
+      <ShoppingCartIcon color={"#fff"} height={"100%"} />
+
+      <div
+        className={clsx(
+          "inline-flex absolute -top-2 -right-2 justify-center items-center w-6 h-6",
+          "text-xs font-bold",
+          "text-white bg-red",
+          "rounded-full",
+          "border-2 border-white dark:border-gray-900"
+        )}
+      >
+        {cartItemsCount}
+      </div>
+    </button>
+  );
+}
 
 function Nav() {
-  return <nav></nav>;
+  return (
+    <nav className={"h-[10vh] overflow-hidden flex justify-end p-6 shadow-xl"}>
+      <CartIcon />
+    </nav>
+  );
 }
 
 function Footer() {
