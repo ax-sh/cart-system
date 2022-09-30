83a60b4ea4800fadbb99132edea5fbf5cfb00bee
axm <56112475+ax-sh@users.noreply.github.com>
2022-09-30 17:16:25 +0545

    refactor: add checkout button

--- src/app/features/Cart/index.tsx
+++ src/app/features/Cart/index.tsx
@@ -15,22 +15,31 @@ function Cart() {
   const dispatch = useDispatch();
   const cartItems = useSelector((state: RootState) => state.cart.cartItems);
   return (
-    <section className={"container mx-auto grid grid-cols-3 gap-5"}>
-      {cartItems.map((item, index) => {
-        return (
-          <div
-            key={index}
-            className={"h-20 rounded-md border-2 border-white p-2"}
-          >
-            <div className={"flex justify-end"}>
-              <Delete onClick={() => dispatch(removeFromCart(item.id))} />
+    <section className={"container mx-auto "}>
+      <div className={"grid grid-cols-3 gap-5"}>
+        {cartItems.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className={"h-20 rounded-md border-2 border-white p-2"}
+            >
+              <div className={"flex justify-end"}>
+                <Delete onClick={() => dispatch(removeFromCart(item.id))} />
+              </div>
+              <div className={"px-5 text-xl"}>
+                {item.name} {item.id}
+              </div>
             </div>
-            <div className={"px-5 text-xl"}>
-              {item.name} {item.id}
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
+      <div className={"flex justify-end"}>
+        <button
+          className={"border-white border-2 px-12 py-2 rounded-md text-4xl"}
+        >
+          Checkout
+        </button>
+      </div>
     </section>
   );
 }
