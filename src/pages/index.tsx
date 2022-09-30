657f1a803109b6867c8371d01b54cbb208b8dc99
axm <56112475+ax-sh@users.noreply.github.com>
2022-09-30 17:49:02 +0545

    feat: add brand color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
 import { Layout } from "../app/App";
 import ProductView from "../app/features/ProductsView";
+import { addToCart } from "../app/features/Cart/cart.slice";
+import { useDispatch } from "react-redux";
+
+function Hero() {
+  const dispatch = useDispatch();
+  return (
+    <section className={"container mx-auto py-50"}>
+      <div className={"flex flex-col gap-10 items-start"}>
+        <h6 className={"text-[#ffd500] text-4xl"}>Classic Yellow Look</h6>
+        <button
+          className={"bg-[#ffd500] text-[#15151e] px-6 py-2 rounded-md"}
+          onClick={() => dispatch(addToCart("qw"))}
+        >
+          Add To cart
+        </button>
+      </div>
+    </section>
+  );
+}
 
 function Page() {
   return (
     <Layout>
+      <Hero />
       <ProductView />
     </Layout>
   );
