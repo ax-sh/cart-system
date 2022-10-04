89b665b98b9fdae707609e20761ea5862209f003
axm <56112475+ax-sh@users.noreply.github.com>
2022-10-04 20:33:09 +0545

    fix: api multicall

--- src/app/features/ProductsView/index.tsx
+++ src/app/features/ProductsView/index.tsx
@@ -1,35 +1,76 @@
 import { range } from "../../../utils";
 import { faker } from "@faker-js/faker";
-import React from "react";
+import React, { useCallback, useRef } from "react";
 import clsx from "clsx";
+import { useClickAway } from "react-use";
+import { XMarkIcon } from "@heroicons/react/20/solid";
 
 function generateProduct() {
   const name = faker.commerce.product();
-  const image = faker.image.abstract(500, 500, true);
+  const image = faker.image.abstract(2000, 2000, true);
   return { name, image };
+}
+
+function FullScreenProductView({ setEnlarge, image }: any) {
+  return (
+    <div className={"flex-grow flex flex-col z-20"}>
+      <div className={"flex justify-end"}>
+        <button onClick={() => setEnlarge(false)}>
+          <XMarkIcon height={50} />
+        </button>
+      </div>
+      <div className={"grid grid-cols-12 flex-grow px-5 gap-5"}>
+        <img className={"col-span-5"} src={image} />
+        jj
+      </div>
+    </div>
+  );
 }
 
 const ProductItem = ({ name, image }: any) => {
   const [enlarge, setEnlarge] = React.useState(false);
+  const ref = useRef(null);
+  const handler = useCallback(() => {
+    console.log("OUTSIDE CLICKED");
+  }, [enlarge]);
+  useClickAway(
+    ref,
+    () => {
+      handler();
+    },
+    ["click"]
+  );
 
   return (
     <div
+      ref={ref}
       className={clsx(
         "Product-item bg-white/6 rounded-md shadow-xl overflow-hidden transition-all",
+
         enlarge
-          ? "h-screen fixed top-0 grid place-items-center z-10"
+          ? "h-screen fixed top-0 left-0 w-full grid place-items-center z-10"
           : "relative h-90"
       )}
-      onClick={() => setEnlarge((x) => !x)}
     >
       <input className={"hidden"} type={"checkbox"} />
+      {enlarge && (
+        <div className={"absolute h-full w-full z-50 flex"}>
+          <FullScreenProductView setEnlarge={setEnlarge} image={image} />
+        </div>
+      )}
       <div className={"absolute p-8 shadow-inset-xl"}>
-        <h4 className={" text-2xl font-bolder"}>{name}</h4>
+        <h4 className={"text-2xl font-bolder"}>{name}</h4>
       </div>
       <img
-        className={"h-full w-full object-cover"}
+        className={clsx(
+          enlarge && "filter ",
+          "blur-xl h-full w-full object-contain",
+          "hover:scale-100 transform"
+        )}
         alt={"product"}
         src={image}
+        loading={"lazy"}
+        onClick={() => setEnlarge((x) => !x)}
       />
     </div>
   );
